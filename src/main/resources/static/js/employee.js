5880fa79b0042f3cc4c01015e8693464bb57fc17
Zhenghaohong <623751665@qq.com>
2020-08-19 15:42:29 +0800

    人员信息管理Demo 增加时间下拉框

--- src/main/resources/static/js/employee.js
+++ src/main/resources/static/js/employee.js
@@ -1,24 +1,4 @@
-$(function() {
-    function reposition() {
-
-        var modal = $(this),
-            dialog = modal.find('.modal-dialog');
-
-        modal.css('display', 'block');
-        
-        // Dividing by two centers the modal exactly, but dividing by three 
-        // or four works better for larger screens.
-        dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
-    }
-    // Reposition when a modal is shown
-    $('.modal').on('show.bs.modal', reposition);
-
-    // Reposition when the window is resized
-    $(window).on('resize', function() {
-        $('.modal:visible').each(reposition);
-    });
-
-    $(document).ready(function () {
+$(document).ready(function () {
         alert("进入");
         $('#EmployeeTable').bootstrapTable({
             url:"/GetEmployeeInfo",  //请求地址
@@ -26,8 +6,9 @@ $(function() {
             striped : true, //是否显示行间隔色
             pageNumber : 1, //初始化加载第一页
             pagination : true,//是否分页
+            search:true,
             sidePagination : 'client',//server:服务器端分页|client：前端分页
-            pageSize : 5,//单页记录数
+            pageSize : 10,//单页记录数
             pageList : [ 5, 10],//可选择单页记录数
             showRefresh : true,//刷新按钮
             columns : [ {
@@ -47,38 +28,31 @@ $(function() {
                 field : 'deptname',
                 sortable : true
             }]
-        })
-    })
+        });
 
-});
+    $('#mytable').datepicker({
+        weekStart: 0, //一周从哪一天开始
+        todayBtn: 1, //
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        forceParse: 0,
+        showMeridian: 1
+    });
+    });
 
-$(document).load(function () {
-    alert("进入2");
-    $('#EmployeeTable').bootstrapTable({
-        url:"/GetEmployeeInfo",  //请求地址
-        striped : true, //是否显示行间隔色
-        pageNumber : 1, //初始化加载第一页
-        pagination : true,//是否分页
-        sidePagination : 'client',//server:服务器端分页|client：前端分页
-        pageSize : 10,//单页记录数
-        pageList : [ 5, 10],//可选择单页记录数
-        showRefresh : true,//刷新按钮
-        columns : [ {
-            title : '编号',
-            field : 'userid',
-            sortable : true
-        }, {
-            title : '姓名',
-            field : 'username',
-            sortable : true
-        }, {
-            title : '性别',
-            field : 'nsex',
-            sortable : true
-        },{
-            title : '部门',
-            field : 'deptname',
-            sortable : true
-        }]
-    })
-})
+
+    $('#mytable').datepicker({
+        weekStart: 0, //一周从哪一天开始
+        todayBtn: 1, //
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        forceParse: 0,
+        showMeridian: 1
+    });
+
+
+
+
+
